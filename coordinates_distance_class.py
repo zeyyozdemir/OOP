f53dc223f2af4228a382c71b2c031b7fe04a3958
zeynep ozdemir <zeyynepozdemir.py@gmail.com>
2024-12-05 20:00:46 +0300

    coordinates_distance_class.py

--- coordinates_distance_class.py
+++ coordinates_distance_class.py
@@ -1,3 +1,7 @@
+# the class Point takes in the coordinate x,y,
+# First method is plot_point that prints the position of point.
+# Second method is calculate_dist which takes in x and y from another point, and returns the distance calculated between two points.
+
 import math
 
 class Point():
@@ -11,8 +15,10 @@ class Point():
     def calculate_dist(self,x1,y1):
         distance = math.sqrt((self.x-x1)**2 + (self.y-y1)**2)
         print(distance)
+        
 
-point1 = Point(3,4)
-point1.plot_pint()
-point1.calculate_dist(7,4)
+point1 = Point(3,4) ## point1 is an object of this class
+point1.plot_point()
+
+point1.calculate_dist(7,4) ## with method calculate_dist this calculates the distance between (7,4) and point1
 
